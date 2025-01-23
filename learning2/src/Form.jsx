80de75fe1160d6aa63948637bb8f200adeb7ffa1
Ajithkannan A <ajithkannan.10711984@ltimindtree.com>
2025-01-23 14:42:19 +0530

    changed css in form.jsx

--- learning2/src/Form.jsx
+++ learning2/src/Form.jsx
@@ -1,7 +1,7 @@
 import {React, useState} from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-// import './Form.css'
+
 
 const Form =(props) =>{
     const {loggedIn} = props
